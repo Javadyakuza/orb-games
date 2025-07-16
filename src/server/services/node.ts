a7f9911c3e21da79ac3e1aad2b80a685a28d8613
javadyakuza <javadyakuzaa@gmail.com>
2025-07-16 05:52:50 +0330

    Feat: Backend integration

---
+++ src/server/services/node.ts
@@ -0,0 +1,8 @@
+// This node will continuously listen new events from the escrow contract 
+
+
+async function listenToEvents() {
+    // Listen to events from the escrow contract
+    // This will be a websocket connection
+}
+
